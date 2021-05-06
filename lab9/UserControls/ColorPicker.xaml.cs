65660a79a17db6299083824c971f7a5f740ad648
Lil Devil <themax.06y@gmail.com>
2021-05-06 03:35:38 +0300

    9 лабораторная

---
+++ lab9/UserControls/ColorPicker.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab9.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker : UserControl
+    {
+        public static DependencyProperty ColorProperty;
+
+        public static DependencyProperty RedProperty;
+        public static DependencyProperty GreenProperty;
+        public static DependencyProperty BlueProperty;
+
+        static ColorPicker()
+        {
+            ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChanged)));
+
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.PropertyChangedCallback = new PropertyChangedCallback(OnColorRGBChanged);
+            metadata.CoerceValueCallback = new CoerceValueCallback(CorrectValue);
+
+            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(OnColorRGBChanged, CorrectValue), new ValidateValueCallback(ValidateValue));
+            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(OnColorRGBChanged, CorrectValue), new ValidateValueCallback(ValidateValue));
+            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(OnColorRGBChanged, CorrectValue), new ValidateValueCallback(ValidateValue));
+        }
+
+        private static object CorrectValue(DependencyObject d, object baseValue)
+        {
+            byte currentValue = (byte)baseValue;
+            if (currentValue > 255)
+                return 255;
+            return currentValue;
+        }
+
+        private static bool ValidateValue(object value)
+        {
+            byte currentValue = (byte)value;
+            if (currentValue >= 0)
+                return true;
+            return false;
+        }
+
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public byte Red
+        {
+            get { return (byte)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+        public byte Green
+        {
+            get { return (byte)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+        public byte Blue
+        {
+            get { return (byte)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+
+        private static void OnColorRGBChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ColorPicker colorPicker = (ColorPicker)sender;
+            Color color = colorPicker.Color;
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            else if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+            else if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+
+            colorPicker.Color = color;
+        }
+
+        private static void OnColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor = (Color)e.NewValue;
+            ColorPicker colorPicker = (ColorPicker)sender;
+            colorPicker.Red = newColor.R;
+            colorPicker.Green = newColor.G;
+            colorPicker.Blue = newColor.B;
+
+        }
+
+        public ColorPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ lab9/UserControls/Events.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab9.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Events.xaml
+    /// </summary>
+    public partial class Events : UserControl
+    {
+        public Events()
+        {
+            InitializeComponent();
+        }
+
+        private void Control_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            textBlock1.Text = textBlock1.Text + "sender: " + sender.ToString() + "\n";
+            textBlock1.Text = textBlock1.Text + "source: " + e.Source.ToString() + "\n\n";
+        }
+    }
+}
